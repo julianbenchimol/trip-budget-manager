17c098aa1fe24a138f515ed0229c757bb21a51fc
gitkcb <kcharlesbell@gmail.com>
2023-03-06 18:53:50 -0500

    added login and addUser mutations in resolver.js

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -11,7 +11,31 @@ const resolvers = {
       // Populate the professor subdocument when querying for classes
       return await User.find({});
     }
+    },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user= await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     }
+  }
 };
 
   
