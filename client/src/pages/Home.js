8fe04dcec732483be5c333225b93b73c7a6a1746
Alex Frankhouser <freesoul423@gmail.com>
2023-03-06 14:18:26 -0500

    updated the Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,16 +5,14 @@ import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Home = () => {
-   
     return (
-        <div className = "container">
-            <h2>Start your next trip here</h2>
-        <div>
-            <label className= "form-label"></label>
-            <input className= "form-control" placeholder="Search here"></input>
-            <button type="submit" className="btn btn-primary">Search</button>
-        </div>
-        <div>
+        <div className = "container-fluid">
+            <h2 className='text-center mb-4 mt-5'>Start your next trip here!</h2>
+                <div className='d-flex mb-4' role='search'>
+                    <input className= "form-control" placeholder="Search here" aria-label='Search'></input>
+                    <button type="submit" className="btn btn-primary ms-2">Search</button>
+                </div>
+        <div className='mb-4'>
             <h3>Welcome to ____ conveniently plan and budget for you trip</h3>
         </div>
 
