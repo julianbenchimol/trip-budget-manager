9c41725c501579b4de81f68b61f9a9664740364f
Alex Frankhouser <freesoul423@gmail.com>
2023-03-06 17:45:18 -0500

    updated navbar styling active and hover link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,13 +7,13 @@ import Auth from '../utils/auth';
 const Home = () => {
     return (
         <div className = "container-fluid">
-            <h2 className='text-center mb-4 mt-5'>Start your next trip here!</h2>
+            <h2 className='text-center mb-4 mt-5 fs-1'>Start your next trip here!</h2>
                 <div className='d-flex mb-4' role='search'>
                     <input className= "form-control" placeholder="Search here" aria-label='Search'></input>
                     <button type="submit" className="btn btn-primary ms-2">Search</button>
                 </div>
-        <div className='mb-4 text-center'>
-            <h3>Welcome to ____ conveniently plan and budget for you trip</h3>
+        <div className='mb-4 text-center mt-5'>
+            <h3 className='fs-3'>Welcome to the one stop shop for trip planing! We are here to help you conveniently plan and budget for your next trip.</h3>
         </div>
 
         </div>
