8c1dc35dddd0d72d4e7e2136cc6792ba706864c9
Alex Frankhouser <freesoul423@gmail.com>
2023-03-06 19:43:46 -0500

    updated styling for SignUp page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ function Footer(){
     return (
     <div className="bg-dark d-flex flex row fixed-bottom" id="footer">
         <a href="mailto:freesoul423@gmail.com"className="text-center fs-5 text-light mt-2">
-            <p className="text-center fs-5 text-light">Click here to contact us here!</p>
+            <p className="text-center fs-5 text-light">Click here to contact us!</p>
         </a>
     </div>
     );}
