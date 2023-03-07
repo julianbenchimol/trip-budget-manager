23d2638f9af25bb7ff600e2507db2e1f78ddade9
Alex Frankhouser <freesoul423@gmail.com>
2023-03-07 18:13:54 -0500

    centered buttons in  login/logout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,16 +8,18 @@ const Home = () => {
     return (
         <div className = "container-fluid">
             <h2 className='text-center mb-4 mt-5 fs-1'>Start your next trip here!</h2>
+            
+            {/* Search Bar */}
                 <div className='d-flex mb-4' role='search'>
                     <input className= "form-control" placeholder="Search here" aria-label='Search'></input>
                     <button type="submit" className="btn btn-primary ms-2">Search</button>
+                
                 </div>
-        <div className='mb-4 text-center mt-5'>
-            <h3 className='fs-3'>Welcome to the one stop shop for trip planing! We are here to help you conveniently plan and budget for your next trip.</h3>
+            {/* Welcome Text */}
+            <div className='mb-4 text-center mt-5'>
+                <h3 className='fs-3'>Welcome to the one stop shop for trip planing! We are here to help you conveniently plan and budget for your next trip.</h3>
+            </div>
         </div>
-
-        </div>
-
     )
 }
 export default Home
