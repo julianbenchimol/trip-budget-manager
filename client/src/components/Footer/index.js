527d1125c833625a3908ba9d08004099cb8ac2be
Alex Frankhouser <freesoul423@gmail.com>
2023-03-06 19:29:45 -0500

    centered footer text

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,8 @@ import React from "react";
 function Footer(){
     return (
     <div className="bg-dark d-flex flex row fixed-bottom" id="footer">
-        <a href="mailto:freesoul423@gmail.com"className="text-center fs-5 text-light">
-            <p className="text-center fs-4 text-light">Click here to contact us here!</p>
+        <a href="mailto:freesoul423@gmail.com"className="text-center fs-5 text-light mt-2">
+            <p className="text-center fs-5 text-light">Click here to contact us here!</p>
         </a>
     </div>
     );}
