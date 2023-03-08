138d3af14d1364096d78b8e92d4a88af65e7e0a4
Alex Frankhouser <freesoul423@gmail.com>
2023-03-07 20:48:09 -0500

    added the link to db to the connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/trip-budget-manager",
+  process.env.MONGODB_URI ||
+    "mongodb+srv://A-Frankhouser:VTObaE6nwN6ncUAb@cluster0.ognhcdo.mongodb.net/Pack&Go?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
