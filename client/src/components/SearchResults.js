3bb3c170aea49fd50707d5b335ef74602c0e1ab2
Alex Frankhouser <freesoul423@gmail.com>
2023-03-08 14:33:31 -0500

    added hover effect to navbar items

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -25,9 +25,9 @@ const SearchResults = () =>{
   return(
     <div>
       <div className="container-fluid">
-        <div className="mb-4" role='search'>
+        <div className="mb-4 " role='search'>
           <form onSubmit={handleFormSubmit} className='d-flex'>
-            <input name="query" value={query} onChange={queryHandler} id = "location-value" className="form-control" placeholder="Start your search here"/>
+            <input name="query" value={query} onChange={queryHandler} id = "location-value" className="form-control" placeholder="Start your search here!"/>
             <button type = "submit" className="btn btn-primary ms-2">Search</button>
           </form>
         </div>
